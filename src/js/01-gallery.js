05eff7966d708e01cc5d26998ec66c797ad1bd20
Artem Petlia <pak@servers.com>
2023-04-18 01:22:12 +0300

    finished task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,20 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
+import { createGallaryListHTML } from './function.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallaryDivRef = document.querySelector('.gallery');
+
+const gallaryListMarkup = createGallaryListHTML(galleryItems);
+
+gallaryDivRef.innerHTML = gallaryListMarkup;
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt',
+  captionClass: 'text--align-center',
+});
---
+++ src/js/function.js
@@ -0,0 +1,17 @@
+export function createGallaryListHTML(items) {
+  return items
+    .map(item => {
+      const { preview, original, description } = item;
+
+      const template = `
+      <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+      </li>
+      `;
+
+      return template;
+    })
+    .join('');
+}
