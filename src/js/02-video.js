e0bcd856d2a921a48554a4c6804e61677aeaf9b5
Artem Petlia <pak@servers.com>
2023-04-18 15:37:44 +0300

    done task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+
+const player = new Vimeo(iframe);
+
+currentTime = localStorage.getItem('currentVideoTime');
+
+if (currentTime) {
+  player.setCurrentTime(currentTime);
+}
+
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
+
+function onTimeUpdate(evt) {
+  localStorage.setItem('currentVideoTime', evt.seconds);
+}
